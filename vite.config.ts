3390a8dee582d6a3d189c58eca76e40143bcf613
vitin-1 <vm1686460@gmail.com>
2026-04-22 01:56:35 -0300

    Fixing camera

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import fs from "fs";
 import { componentTagger } from "lovable-tagger";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -21,7 +20,7 @@ export default defineConfig(({ mode }) => ({
         }
       : undefined,
   },
-  plugins: [react(), basicSsl(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
